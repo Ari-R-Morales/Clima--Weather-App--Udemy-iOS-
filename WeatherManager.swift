7ff81e85a0e43488b4adaa07a951dfe3648e554e
Ari-R-Morales <arm379@scarletmail.rutgers.edu>
2019-09-13 11:15:24 +0100

    Completed the project

---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Arianna Morales on 9/14/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Arianna Morales on 9/14/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
